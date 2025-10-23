4ffd54ff343d52732754a3567e2ef011ef390673
hngluc <nguyendanghoangluc@gmail.com>
2025-10-23 22:37:08 +0700

    Fix MoveFile

---
+++ src/test/java/com/example/DownloadManager/DownloadManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DownloadManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DownloadManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
